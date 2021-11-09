126648468cb31d409328f905c2681b67f1da1cbf
danfilof <danila.anfilofyev@gmail.com>
2021-11-09 13:06:30 +0100

    Code partically copied from the video

--- src/main/java/gb/ru/Calculator.java
+++ src/main/java/gb/ru/Calculator.java
@@ -1,4 +1,31 @@
 package gb.ru;
 
-public class Calculator {
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class Calculator extends JFrame {
+
+    public static void main(String[] args) {
+        new Calculator();
+    }
+
+    public Calculator() {
+        setTitle("");
+        setBounds(300,300,300,100);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        final JPanel numberPanel = new JPanel();
+        final GridLayout numberLayout = new GridLayout (4, 3, 10, 10);
+        numberPanel.setLayout(numberLayout);
+
+        for (int i = 0; i < 10; i++) {
+            JButton button = new JButton(String.valueOf(i));
+            numberPanel.add(button);
+        }
+
+        setVisible(true);
+
+    }
 }
