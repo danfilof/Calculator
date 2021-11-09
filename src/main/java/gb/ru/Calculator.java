d13ccd78a2921ac87e417e9ed3af9778b19eb821
danfilof <danila.anfilofyev@gmail.com>
2021-11-09 13:39:47 +0100

    Slightly more code copied

--- src/main/java/gb/ru/Calculator.java
+++ src/main/java/gb/ru/Calculator.java
@@ -3,8 +3,12 @@ package gb.ru;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Calculator extends JFrame {
+
+    private JLabel display;
 
     public static void main(String[] args) {
         new Calculator();
@@ -16,12 +20,30 @@ public class Calculator extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
+        ActionListener actionListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JButton source = (JButton) e.getSource();
+                String text = source.getText();
+                String displayText = display.getText();
+                if (".".equals(text) && displayText.contains(".")) {
+                    return;
+                }
+                if ("0".equals(displayText)) {
+                    displayText = "";
+                }
+                displayText += text;
+                display.setText(displayText);
+            }
+        };
+
         final JPanel numberPanel = new JPanel();
         final GridLayout numberLayout = new GridLayout (4, 3, 10, 10);
         numberPanel.setLayout(numberLayout);
 
         for (int i = 0; i < 10; i++) {
             JButton button = new JButton(String.valueOf(i));
+            button.addActionListener(actionListener);
             numberPanel.add(button);
         }
         numberPanel.add(new JButton("."));
@@ -36,9 +58,13 @@ public class Calculator extends JFrame {
             buttonPanel.add(new JButton(String.valueOf(c)));
         }
 
-
+        display = new JLabel("0");
+        display.setHorizontalAlignment(SwingConstants.RIGHT);
+        display.setFont(new Font("Arial", Font.BOLD, 17));
+        add(display, BorderLayout.NORTH);
         add(numberPanel, BorderLayout.CENTER);
         add(buttonPanel, BorderLayout.SOUTH);
+
 
         setVisible(true);
 
