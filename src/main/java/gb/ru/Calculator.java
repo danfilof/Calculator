74560d3390330538e9235b8ef2c0701ae3c80c8a
danfilof <danila.anfilofyev@gmail.com>
2021-11-09 13:20:45 +0100

    More code copied, all main buttons created

--- src/main/java/gb/ru/Calculator.java
+++ src/main/java/gb/ru/Calculator.java
@@ -12,7 +12,7 @@ public class Calculator extends JFrame {
 
     public Calculator() {
         setTitle("");
-        setBounds(300,300,300,100);
+        setBounds(300,300,500,500);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
@@ -24,6 +24,21 @@ public class Calculator extends JFrame {
             JButton button = new JButton(String.valueOf(i));
             numberPanel.add(button);
         }
+        numberPanel.add(new JButton("."));
+        numberPanel.add(new JButton("+/-"));
+
+        JPanel buttonPanel = new JPanel();
+        GridLayout buttonLayout = new GridLayout(2, 3, 10, 10);
+        buttonPanel.setLayout(buttonLayout);
+
+
+        for (char c : "+-*/C=".toCharArray()) {
+            buttonPanel.add(new JButton(String.valueOf(c)));
+        }
+
+
+        add(numberPanel, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);
 
         setVisible(true);
 
